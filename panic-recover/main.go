01ea898bc61d456a799a91c9f2d599e7510b4390
Renato Lima <natenho@gmail.com>
2021-09-29 13:16:46 -0300

    Improve recover adding go routine and defer order example

--- panic-recover/main.go
+++ panic-recover/main.go
@@ -14,16 +14,43 @@ func main() {
 	x := 7
 	y := 0
 
-	for  {
+	for {
 		foo(x, y)
+		go bar(x, y)
+		go bar_fixed(x, y)
 		time.Sleep(1 * time.Second)
 	}
 }
 
 func foo(x int, y int) {
 	defer func() {
+		log.Printf("(foo) Eu tambem sou executado (ultimo)")
+	}()
+	defer func() {
 		if r := recover(); r != nil {
-			log.Printf("Ocorreu panic mas tudo vai continuar funcionando!! %v \n", r)
+			log.Printf("(foo) Ocorreu panic mas tudo vai continuar funcionando!! %v \n", r)
+		}
+	}()
+	defer func() {
+		log.Printf("(foo) Eu tambem sou executado (segundo)")
+	}()
+	defer func() {
+		log.Printf("(foo) Eu tambem sou executado (primeiro)")
+	}()
+	divisionByZero := x / y
+	log.Printf("%v", divisionByZero)
+}
+
+func bar(x int, y int) {
+	log.Printf("(bar) go routines nao sobem panic para o main, devem ter seu proprio defer!!")
+	divisionByZero := x / y
+	log.Printf("%v", divisionByZero)
+}
+
+func bar_fixed(x int, y int) {
+	defer func() {
+		if r := recover(); r != nil {
+			log.Printf("(bar_fixed) Ocorreu panic mas tudo vai continuar funcionando!! %v \n", r)
 		}
 	}()
 	divisionByZero := x / y
